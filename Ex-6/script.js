e3484feadd1d1ff491e9f9e1b40eae8e26e0b715
taynara almeida <rodrigues.a.tay@gmail.com>
2021-11-03 17:35:28 -0300

    ,

--- Ex-6/script.js
+++ Ex-6/script.js
@@ -1,20 +1,20 @@
-var frmVeiculo
-var frmṔreco
+var frmModelo
+var frmValor
 var divMostrar
 var valorParcela
 document.getElementById('mostrarInfo').style.display = "none";
 
 btnCalc.addEventListener('click', function() {
-    frmVeiculo = modelo.value
-    frmPreco = valor.value / 2
+    frmModelo = modelo.value
+    frmValor = valor.value / 2
     valorParcela = (frmValor / 60) * 1.7
     divMostrar = document.getElementById('mostrarInfo')
     document.getElementById('mostrarInfo').style.display = "block";
     divMostrar.innerHTML = `
         <h2>
-            <p><div>Modelo: <span class="campo">${frmVeiculo}</span></div></p>
-            <p><div>Entrada de <span class="campo">R$ ${frmPreco.toFixed(2)}</span></div></p>
+            <p><div>Modelo: <span class="campo">${frmModelo}</span></div></p>
+            <p><div>Entrada de <span class="campo">R$ ${frmValor.toFixed(2)}</span></div></p>
             <p><div>+60 de <span class="campo">R$ ${valorParcela.toFixed(2)}</span></div></p>
         </h2>
     `
-})x
+})
