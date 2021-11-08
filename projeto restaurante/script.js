9693c807ed8da4b8b9d8167550a9c81357fe7f5d
taynara almeida <rodrigues.a.tay@gmail.com>
2021-11-08 17:17:15 -0300

    .

--- projeto restaurante/script.js
+++ projeto restaurante/script.js
@@ -1,4 +1,19 @@
-var frmPrato
-var frmValor
 var divMostrar
-var valorPrato
+var frmPreco
+var frmPeso
+var valorfinal
+
+
+btnCalc.addEventListener('click', function() {
+    frmPreco = precoporkg.value
+    frmPeso = peso.value
+    frmPeso = frmPeso * 0.001
+    valorfinal = (frmPeso * frmPreco).toFixed(2)
+
+    divMostrar = document.getElementById('mostrarInfo')
+    divMostrar.innerHTML = `
+    <h2>
+        <br>Valor a ser pago no prato é: R$ ${valorfinal}
+    </h2>
+    `
+})
